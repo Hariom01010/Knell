933318845914b4eb126dd84a15113d0e371bd26a
Hariom Vashista <rishivashista4@gmail.com>
2025-04-09 21:50:29 +0530

    Fix deployment issues

--- src/app/service-provider/page.tsx
+++ src/app/service-provider/page.tsx
@@ -6,7 +6,7 @@ import { useState } from "react"
 import { RxCross2 } from "react-icons/rx";
 
 interface Provider {
-    id: string;
+    id: number;
     imgSrc: string;
     name: string;
     occupation: string;
@@ -29,7 +29,7 @@ function Page() {
   return (
     <div className="grid grid-rows-1 md:grid-rows-2 grid-cols-2 gap-6 justify-center justify-items-center">
       {SERVICE_PROVIDER.map((provider)=>(
-        <div className="bg-darkBackground text-darkTextColor m-5 p-5 rounded-md w-[60%] h-max cursor-pointer flex flex-col items-center" key={provider.id} onClick={()=>handleCardClick(provider)}>
+        <div className="bg-darkBackground text-darkTextColor m-5 p-5 rounded-md w-[60%] h-max cursor-pointer flex flex-col items-center" key={provider.id} onClick={()=>handleCardClick(provider)} onKeyDown={(e) => e.key === 'Enter' && handleCardClick(provider)} role="button" tabIndex={0}>
             <Image src={provider.imgSrc} width={150} height={150} className="rounded-md" alt={`${provider.name}-provider-profile-pic`}/>
             <h1 className="text-2xl font-bold pt-3">{provider.name}</h1>
             <h2 className="pb-3">{provider.occupation}</h2>
